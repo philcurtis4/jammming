94564d05605a194fbf05b1cd890909e7b29f90b2
philcurtis422 <philcurtis422@gmail.com>
2023-12-18 17:00:19 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="Title">
+        <h1>
+          <span id='whiteLetters'>Ja</span>
+          <span id='purpleLetters'>mmm</span>
+          <span id='whiteLetters'>ing</span>
+        </h1>
+      </div>
+
+        
+        <div class="image"></div>
+      
     </div>
+    
+
+
+    
   );
 }
 
