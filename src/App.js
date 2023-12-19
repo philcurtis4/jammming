dd59caba0afa6383610cf23cc027a0261a0ea29c
philcurtis422 <philcurtis422@gmail.com>
2023-12-19 12:43:34 -0500

    add search bar

--- src/App.js
+++ src/App.js
@@ -12,9 +12,23 @@ function App() {
         </h1>
       </div>
 
+      <div class="body">
+      <div id="cover">
+        <form method="get" action="">
+          <div class="tb">
+            <div class="td"><input type="text" placeholder="Search" required/></div>
+              <div class="td" id="s-cover">
+                <button type="submit">
+                <div id="s-circle"></div>
+                <span></span>
+                </button>
+              </div>
+          </div>
+        </form>
+      </div>
+          
+      </div>
         
-        <div class="image"></div>
-      
     </div>
     
 
